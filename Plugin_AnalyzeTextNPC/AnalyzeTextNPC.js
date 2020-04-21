1c14d52001b55454b415bc21c05d0dedbe876f7c
RYBA549 <64070363+RYBA549@users.noreply.github.com>
2020-04-21 18:27:03 +0900

    Add files via upload

---
+++ Plugin_AnalyzeTextNPC/AnalyzeTextNPC.js
@@ -0,0 +1,67 @@
+
+/*--------------------------------------------------------------------------
+  
+NPCs will also be able to use control characters
+
+Up to number 3 is available.
+Copy and add more classes if you need to.
+
+
+update
+20/04/21　create
+
+
+Version
+　SRPG Studio Version:1.210
+
+
+MIT License Copyright (c) 2020 RYBA
+  
+--------------------------------------------------------------------------*/
+var RYBA = RYBA || {};
+RYBA.DataVariable = RYBA.DataVariable || {};
+RYBA.DataVariable.Np1 = defineObject(BaseDataVariable,
+{
+    getList: function() {
+        return root.getBaseData().getNpcList(0);
+    },
+    
+    getKey: function() {
+        var key = /\\np2\[(\d+)\]/;
+        
+        return key;
+    }
+});
+RYBA.DataVariable.Np2 = defineObject(BaseDataVariable,
+{
+    getList: function() {
+        return root.getBaseData().getNpcList(1);
+    },
+    
+    getKey: function() {
+        var key = /\\np2\[(\d+)\]/;
+        
+        return key;
+    }
+});
+RYBA.DataVariable.Np3 = defineObject(BaseDataVariable,
+{
+    getList: function() {
+        return root.getBaseData().getNpcList(2);
+    },
+    
+    getKey: function() {
+        var key = /\\np3\[(\d+)\]/;
+        
+        return key;
+    }
+});
+(function() {
+    var alias = VariableReplacer._configureVariableObject;
+	VariableReplacer._configureVariableObject = function(groupArray) {
+        alias.call(this,groupArray);
+        groupArray.appendObject(RYBA.DataVariable.Np1);
+        groupArray.appendObject(RYBA.DataVariable.Np2);
+	groupArray.appendObject(RYBA.DataVariable.Np3);
+	}
+})();
