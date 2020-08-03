de0df61d24fe9e89719997636e22e29c45a4cb73
RYBA549 <64070363+RYBA549@users.noreply.github.com>
2020-08-03 18:13:33 +0900

    Add files via upload

---
+++ SkillHpCheker/SkillHpCheker.js
@@ -0,0 +1,103 @@
+/*--------------------------------------------------------------------------
+  
+You can fine-tune the target HP for the skills.
+
+1.support skills : RYBA_CondSupportHpRate
+2.active skills : RYBA_CondPlayerHpRate or RYBA_CondEnemyHpRate
+
+Custom Parameter Setting Example.
+
+For example 1.
+HP less than half a point.
+{
+	RYBA_CondSupportHpRate:{
+		minHp:0,
+		maxHp:50
+	}
+}
+
+For example 2.
+HP remaining at least 80%.
+{
+	RYBA_CondSupportHpRate:{
+		minHp:80,
+		maxHp:100
+	}
+}
+
+update
+20/08/03Å@create
+Version
+Å@SRPG Studio Version:1.215
+URL
+https://github.com/RYBA549/RYBA_SrpgStudio
+MIT License Copyright (c) 2020 RYBA(îMë—ãõ)
+  
+--------------------------------------------------------------------------*/
+var RYBA = RYBA || {};
+RYBA.UnitHpChecker = {
+    isTargetHp:function(targetUnit, conditionParam){
+        var maxHp = ParamBonus.getMhp(targetUnit);
+        var currentHp = targetUnit.getHp();
+        
+        return this.isBaseTargetHp(maxHp, currentHp, conditionParam);
+    },
+    
+    isBaseTargetHp:function(maxHp, currentHp, conditionParam){
+        var baseHP = 100;
+        baseHp = maxHp * (conditionParam.minHp / 100 );
+        if (currentHp < baseHp) {
+            return false;
+        }
+        baseHp = maxHp * (conditionParam.maxHp / 100 );
+        if (currentHp > baseHp) {
+            return false;
+        }
+        
+        return true;
+
+    }
+};
+(function() {
+
+var alias1 = SkillRandomizer._isSkillInvokedInternal;
+SkillRandomizer._isSkillInvokedInternal = function(active, passive, skill) {
+    var result = alias1.call(this,active,passive,skill);
+    if( !result ){
+        return false;
+    }
+
+    var playerHpRate = skill.custom.RYBA_CondPlayerHpRate;
+	if(playerHpRate != null){
+		if(!RYBA.UnitHpChecker.isTargetHp(active, playerHpRate )){
+			return false;
+		}
+    }
+
+	var enemyHpRate = skill.custom.RYBA_CondEnemyHpRate;
+	if(enemyHpRate != null){
+		if(!RYBA.UnitHpChecker.isTargetHp(passive, enemyHpRate )){
+			return false;
+		}
+    }
+
+    return result;
+}
+
+
+SupportCalculator._isSupportable = function(unit, targetUnit, skill) {
+	if (targetUnit === null) {
+		targetUnit = unit;
+	}
+	
+	var result = skill.getTargetAggregation().isCondition(targetUnit);
+	if (result) {
+		var enemyHpRate = skill.custom.RYBA_CondSupportHpRate;
+		if(enemyHpRate != null){
+			result = RYBA.UnitHpChecker.isTargetHp(targetUnit, enemyHpRate );
+		}
+	}
+	return result;
+};
+
+})();
