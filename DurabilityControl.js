2536ff057d9c02b28718317585207933f509a73a
RYBA549 <64070363+RYBA549@users.noreply.github.com>
2024-10-25 10:33:52 +0900

    Add files via upload

---
+++ DurabilityControl.js
@@ -0,0 +1,67 @@
+
+/**
+ * マップクリア後にストックと自軍の持ち物の耐久を全回復するプラグイン（たぶん）
+ */
+var RYBA = RYBA || {};
+
+RYBA.DurabilityControl = {
+
+    resetDurability:function(item){
+        var max = item.getLimitMax();
+        item.setLimit(max);
+    },
+
+    unitResetDurability:function(unit){
+        var i, item;
+        var count = UnitItemControl.getPossessionItemCount(unit);
+        
+        for (i = 0; i < count; i++) {
+            item = UnitItemControl.getItem(unit, i);
+            if( item == null ){
+                continue;
+            }
+            this.resetDurability(item);
+        }
+    },
+
+    unitListResetDurability:function(list){
+        var count = list.getCount();
+        
+        for( i = 0; i < count; ++i ){
+            var unit = list.getData(i);
+            this.unitResetDurability(unit);
+        }
+    },
+
+    stockAllItemResetDurability:function(){
+        var count = StockItemControl.getStockItemCount();
+        var i;
+        for( i = 0; i < count; ++i ){
+          var item = StockItemControl.getStockItem(i);
+          this.resetDurability(item);
+        }
+    },
+
+    playerUnitAllResetDurability:function(){
+        var list = PlayerList.getAliveDefaultList();
+        this.unitListResetDurability(list);
+    }
+};
+
+/**
+ * 最後にスクリプトの実行のコード実行で以下のコードを実行する事でも回復できます。
+ * RYBA.DurabilityControl.stockAllItemResetDurability();
+ * RYBA.DurabilityControl.playerUnitAllResetDurability();
+ */
+/**
+ * 耐久回復タイミングを制御したい場合、
+ * 以下のコードをコメントアウトしてください。
+ */
+(function() {
+    var alias = BattleResultScene._doEndAction;
+    BattleResultScene._doEndAction = function() {
+        alias.call(this);
+        RYBA.DurabilityControl.stockAllItemResetDurability();
+        RYBA.DurabilityControl.playerUnitAllResetDurability();
+	}
+})();
